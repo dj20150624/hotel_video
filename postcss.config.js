3ff838551eb8535e64aa92299a801f4679af46d1
dj <1655379381@qq.com>
2019-02-21 17:46:28 +0800

    'js'

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    'autoprefixer': {
+      browsers: ['Android >= 4.0', 'iOS >= 7']
+    },
+    'postcss-pxtorem': { rootValue: 16, propList: [ '*', '!font-size' ], minPixelValue: 2 }
+  }
+}
